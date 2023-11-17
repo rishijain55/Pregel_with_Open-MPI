647cf72c7d1b0c52b64c2e199aba7e763196e358
rishijain55 <rishijain55@gmail.com>
2023-11-17 12:48:43 +0530

    pbgl and custom bfs completed

--- pbgl_bfs/bfs.cpp
+++ pbgl_bfs/bfs.cpp
@@ -1,15 +1,3 @@
-// Copyright (C) 2004-2008 The Trustees of Indiana University.
-
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-//  Authors: Douglas Gregor
-//           Andrew Lumsdaine
-
-// Example usage of breadth_first_search algorithm
-
-// Enable PBGL interfaces to BGL algorithms
 #include <boost/graph/use_mpi.hpp>
 
 // Communicate via MPI
@@ -33,6 +21,7 @@
 // Standard Library includes
 #include <fstream>
 #include <string>
+#include <set>
 
 #ifdef BOOST_NO_EXCEPTIONS
 void
@@ -47,25 +36,44 @@ using namespace boost;
 using boost::graph::distributed::mpi_process_group;
 
 /* An undirected graph with distance values stored on the vertices. */
-typedef adjacency_list<vecS, distributedS<mpi_process_group, vecS>, undirectedS,
+typedef adjacency_list<vecS, distributedS<mpi_process_group, vecS>, directedS,
                        /*Vertex properties=*/property<vertex_distance_t, std::size_t> >
   Graph;
+
+
+
+void get_graph(int numWorkers,int N, Graph &g){
+    
+    for(int workerId=0;workerId<numWorkers;workerId++){
+        int start = (N/(numWorkers))*(workerId);
+        int end = start + N/(numWorkers);
+        if(workerId==numWorkers-1) end = N;
+        for(int i=start;i<end;i++){
+            std::set<int> adj;
+            while(adj.size()!=10){
+                int t = rand()%N;
+                if(t==i) continue;
+                adj.insert(t);
+            }
+            std::vector<int> targets(adj.begin(),adj.end());
+            for(auto t:targets){
+                add_edge(vertex(i, g), vertex(t, g), g);
+            }
+        }
+    };
+    return;
+}
 
 int main(int argc, char* argv[])
 {
   boost::mpi::environment env(argc,argv);
 
-  // Parse command-line options
-  const char* filename = "weighted_graph.gr";
-  if (argc > 1) filename = argv[1];
-
-  // Open the METIS input file
-  std::ifstream in(filename);
-  graph::metis_reader reader(in);
-
-  // Load the graph using the default distribution
-  Graph g(reader.begin(), reader.end(),
-          reader.num_vertices());
+    int numWorkers =  boost::mpi::communicator().size();
+    int workerId = boost::mpi::communicator().rank();
+    // Generate a simple graph
+    int numVertices = 20;
+    Graph g(numVertices);
+    get_graph(numWorkers,numVertices,g);
 
   // Get vertex 0 in the graph
   graph_traits<Graph>::vertex_descriptor start = vertex(0, g);
@@ -85,27 +93,8 @@ int main(int argc, char* argv[])
     (g, start,
      visitor(make_bfs_visitor(record_distances(distance, on_tree_edge()))));
 
-  // Output a Graphviz DOT file
-  std::string outfile;
-
-  if (argc > 2)
-    outfile = argv[2];
-  else {
-    outfile = filename;
-    size_t i = outfile.rfind('.');
-    if (i != std::string::npos)
-      outfile.erase(outfile.begin() + i, outfile.end());
-    outfile += "-bfs.dot";
-  }
-
-  if (process_id(process_group(g)) == 0) {
-    std::cout << "Writing GraphViz output to " << outfile << "... ";
-    std::cout.flush();
-  }
-  write_graphviz(outfile, g,
-                 make_label_writer(distance));
-  if (process_id(process_group(g)) == 0)
-    std::cout << "Done." << std::endl;
+  // Output the graph to terminal
+  write_graphviz(std::cout, g, make_label_writer(get(vertex_distance, g)));
 
   return 0;
 }
